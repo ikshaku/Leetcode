343714da290b037c901fff05e302b50432fa61ef
Ikshaku Goswami <ig752994@gmail.com>
2023-08-06 15:44:45 -0700

    Update sumRootToLeafNumbers.java

--- Trees/Trees2/sumRootToLeafNumbers.java
+++ Trees/Trees2/sumRootToLeafNumbers.java
@@ -1,1 +1,95 @@
-
+class Solution {
+    
+    int number = 0;
+    int result = 0;
+    
+    class Pair{
+        
+        TreeNode node;
+        int value;
+        
+        public Pair(TreeNode node, int value){
+            
+            this.node = node;
+            this.value = value;
+            
+        }
+        
+        public TreeNode getNode(){
+            
+            return this.node;
+            
+        }
+        
+        public int getValue(){
+            
+            return this.value;
+            
+        }
+        
+    }
+    
+    public int sumNumbers(TreeNode root) {
+        
+        // int result = 0;
+        // int number = 0;
+        
+//         Stack<Pair> stack = new Stack<>();
+        
+//         while(root!=null || !stack.isEmpty()){
+            
+//             while(root!=null){
+                
+//                 //NLR
+//                 number = number * 10 + root.val;
+//                 Pair pair = new Pair(root,number);
+                
+//                 stack.push(pair);
+                
+//                 root=root.left;
+                
+//             }
+            
+//             Pair pair = stack.pop();
+//             root = pair.getNode();
+//             number = pair.getValue();
+            
+//             if(root.left==null && root.right==null){
+                
+//                 result+=number;
+                
+//             }
+            
+//             root = root.right;
+            
+//         }
+        
+        // return result;
+        
+        return helper(root,0);
+        
+    }
+    
+    public int helper(TreeNode root, int number){
+        
+        if(root==null){
+            return 0;
+        }
+        
+        if(root.left==null && root.right==null){
+            
+            result+=number*10 + root.val;
+            return result;
+            
+        }
+        
+        number = number * 10 + root.val;
+        
+        helper(root.left, number);
+        helper(root.right, number);
+        
+        return result;
+        
+    }
+    
+}
