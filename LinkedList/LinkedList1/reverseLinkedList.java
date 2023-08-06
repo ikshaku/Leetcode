3b3b80869d578acc285b2ab972e6dd416a3734a5
Ikshaku Goswami <ig752994@gmail.com>
2023-08-06 16:23:31 -0700

    Update reverseLinkedList.java

--- LinkedList/LinkedList1/reverseLinkedList.java
+++ LinkedList/LinkedList1/reverseLinkedList.java
@@ -1,1 +1,25 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+public class ListNode {
+ // *     int val;
+ // *     ListNode next;
+ // *     ListNode() {}
+ // *     ListNode(int val) { this.val = val; }
+ // *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public ListNode reverseList(ListNode node) {
+  ListNode curr = node;
+  ListNode prev = null;
+  ListNode forward = null;
+  while(curr!=null){
+    forward = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = forward;
+  }
+  return prev;
+}
+  
 
