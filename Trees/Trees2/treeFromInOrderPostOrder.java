6b0d8566a9ee349dff07a1f42943b4878db8b732
Ikshaku Goswami <ig752994@gmail.com>
2023-08-06 15:41:35 -0700

    Update treeFromInOrderPostOrder.java

--- Trees/Trees2/treeFromInOrderPostOrder.java
+++ Trees/Trees2/treeFromInOrderPostOrder.java
@@ -1,1 +1,22 @@
+// Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
+// Output: [3,9,20,null,null,15,7]
 
+
+
+helper(inorder, int isi, int iei, postorder, int psi, int pei){
+
+  int idx = isi;
+  TreeNode root = new TreeNode(postorder[pei]);
+
+  while(inorder[idx]!=postorder[pei]){
+    idx++;
+  }
+
+  int telem = idx - isi;
+
+  root.left = helper(inorder, isi, idx-1, postorder, psi, psi+telem-1);
+  root.right = helper(inorder, idx+1, postorder, psi+telem, pei);
+
+  return root;
+  
+}
